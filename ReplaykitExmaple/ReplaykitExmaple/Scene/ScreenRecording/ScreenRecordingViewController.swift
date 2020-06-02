1d78e008199483733a00d8d8ce41b3b52e4909f8
Bruce <jhjo@maxst.com>
2020-06-02 12:42:05 +0900

    init.

---
+++ ReplaykitExmaple/ReplaykitExmaple/Scene/ScreenRecording/ScreenRecordingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenRecordingViewController.swift
+//  ReplaykitExmaple
+//
+//  Created by jhjo on 2020/06/02.
+//  Copyright © 2020 jhjo. All rights reserved.
+//
+
+import UIKit
+
+class ScreenRecordingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
