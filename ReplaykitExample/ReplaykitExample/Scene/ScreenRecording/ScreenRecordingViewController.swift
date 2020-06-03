fc807d74f68348189218c61c28c58d4da3d0ad8f
jhjo <jhjo.tech@gmail.com>
2020-06-04 00:01:19 +0900

    Add: Sound Plyer And Code cleanup.

--- ReplaykitExample/ReplaykitExample/Scene/ScreenRecording/ScreenRecordingViewController.swift
+++ ReplaykitExample/ReplaykitExample/Scene/ScreenRecording/ScreenRecordingViewController.swift
@@ -6,64 +6,103 @@
 //  Copyright © 2020 jhjo. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 import Photos
 
-class ScreenRecordingViewController: UIViewController {
-    
+extension ScreenRecordingViewController {
     private enum ButtonTyep: Int {
         case start = 0
         case stop = 1
         case pause = 2
+        case audioStart = 3
     }
     
     private struct Color {
         static let pantone19_4052: UIColor = UIColor(displayP3Red: 15, green: 76, blue: 129, alpha: 1.0)
     }
+}
+
+class ScreenRecordingViewController: UIViewController {
+    
+    // MARK: - IBOutlet
     
     @IBOutlet private weak var secondLabel: UILabel!
     @IBOutlet private weak var currentStateLabel: UILabel!
     
+    // MARK: - property
+    
     private let screenRecordingManager = ScreenRecordingManager.shared
     private var timer: Timer?
+    private var audioPlayer: AVAudioPlayer?
     
+    // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getPhotoLibraryAuthorization()
-        
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
-            guard let self = self else { return }
-            guard var second = Int(self.secondLabel.text ?? "0") else { return }
-            second += 1
-            self.secondLabel.text = String(second)
-        }
-        
-        screenRecordingManager.currentStateHandler = { [weak self] state in
-            guard let self = self else { return }
-            DispatchQueue.main.async {
-                self.currentStateLabel.text = state.rawValue
-                if state == .completed, (self.timer?.isValid ?? false) {
-                    self.timer?.invalidate()
-                }
-            }
-        }
+        setupAudioPlayer()
+        setupScreenRecordingManager()
     }
     
-    @IBAction func tapButton(_ sender: UIButton) {
+    // MARK: - IBAction
+    
+    @IBAction private func tapButton(_ sender: UIButton) {
         guard let type = ButtonTyep(rawValue: sender.tag) else { return }
-        
+        print(type)
         switch type {
         case .start:
             screenRecordingManager.setupRecorder()
             screenRecordingManager.start()
+            setupTimer()
         case .pause: break
         case .stop:
             screenRecordingManager.stopRecording()
+        case .audioStart:
+            audioPlayer?.play()
         }
     }
 }
 
+// MARK: - setup
+extension ScreenRecordingViewController {
+    
+    private func setupTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+             guard let self = self else { return }
+             guard var second = Int(self.secondLabel.text ?? "0") else { return }
+             second += 1
+             self.secondLabel.text = String(second)
+         }
+    }
+    
+    private func setupAudioPlayer() {
+        let url = Bundle.main.url(forResource: "Sound", withExtension: ".m4a")!
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+            audioPlayer?.prepareToPlay()
+        } catch (let error) {
+            print("error : \(error)")
+        }
+    }
+    
+    private func setupScreenRecordingManager() {
+        screenRecordingManager.currentStateHandler = { [weak self] state in
+             guard let self = self else { return }
+             DispatchQueue.main.async {
+                 self.currentStateLabel.text = state.rawValue
+                 if state == .completed, (self.timer?.isValid ?? false) {
+                     self.timer?.invalidate()
+                     self.audioPlayer?.stop()
+                 }
+             }
+         }
+    }
+}
+
+// MARK: - Function
 extension ScreenRecordingViewController {
     
     private func getPhotoLibraryAuthorization() {
